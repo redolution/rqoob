d0751e575fd59ef3deedec16bb93d6d80d3cff03
novenary <novenary@kwak.zip>
2024-04-17 15:44:56 +0300

    cli: throw all errors with ?

--- src/main.rs
+++ src/main.rs
@@ -175,7 +175,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 				let mut file = File::open(file)?;
 				let size = file.metadata()?.len();
 				if size > avail as u64 {
-					return Err(Box::new(QoobError::TooBig));
+					Err(QoobError::TooBig)?;
 				}
 				let mut data = Vec::new();
 				file.read_to_end(&mut data)?;
